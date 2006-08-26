f131342980307718ebfb70fb4ae55e0a3f566a5a
alefnula <alefnula@44b417e4-df1b-0410-81cb-fb11499e953d>
2006-08-26 15:54:41 +0000

    S3 implementation completely changed.

--- trunk/s3/service.py
+++ trunk/s3/service.py
@@ -3,14 +3,18 @@ from s3.objects import S3Bucket
 from s3.parsers import parseListBuckets, parseGetBucket, parseGetBucketNames
 
 class S3Service:
-    """S3 Service object"""
+    """
+    S3 Service object
+    
+    Behaves like a dictionary of buckets, with some additional functions.
+    """
     
     def __init__(self, pub_key, priv_key):
         self._s3_conn = S3Connection(pub_key, priv_key)
 
-    def get(self, name):
+    def get(self, name, default=None):
         """
-        Get bucket with the exact name
+        Get bucket with the exact name.
         
         @param name: The name of the bucket
         @type name:  string
@@ -18,11 +22,11 @@ class S3Service:
         @rtype:      S3Bucket or None
         """
         response = self._s3_conn.clone().get()
-        return parseGetBucket(name, response.read(), self._s3_conn)
+        return parseGetBucket(name, response.read(), self._s3_conn, default)
 
     def list(self):
         """
-        List buckets:
+        List all buckets.
         
         @return:       List of buckets associated with the authenticated user
         @rtype:        list
@@ -32,21 +36,20 @@ class S3Service:
 
     def create(self, name):
         """
-        Create a bucket
+        Create a bucket.
         
         @param name: Name for the new bucket
         @type name:  string
         @return:     Returns the newly created bucket
         @rtype:      S3Bucket
         """
-        response = self._s3_conn.clone().put(name)
-##        response.read()
+        self._s3_conn.clone().put(name)
         return S3Bucket(name, self._s3_conn)
 
 
     def delete(self, name):
         """
-        Deletes a bucket
+        Deletes a bucket.
         
         @param name: Name of the queue that should be deleted
         @type  name: string
@@ -55,16 +58,43 @@ class S3Service:
 
 
     def keys(self):
-        """S.keys() -- list buckets, returns just flat list of bucket names"""
+        """
+        Returns a flat list of bucket names.
+        
+        @return: List of bucket names
+        @rtype:  list
+        """
         response = self._s3_conn.clone().get()
         return parseGetBucketNames(response.read())
         
     values = list
 
+    def items(self):
+        """
+        Returns (key, value) pairs, where, keys are bucket names, and values are S3Buckets.
+        
+        @return: List of (bucket name, bucket) pairs
+        @rtype:  list
+        """
+        return zip(self.keys(), self.list())
+
+
+    def has_key(self, key):
+        """
+        Does bucket exist.
+        """
+        return key in self.keys()
+
+
     def __getitem__(self, key):
-        """S.__getitem__(key) -> a bucket instance"""
+        """
+        Get a bucket instance.
+        """
         return self.get(key)
 
+
     def __delitem__(self, key):
-        """S.__delitem__(key) -- delete a bucket"""
+        """
+        Deletes a bucket
+        """
         self.delete(key)
