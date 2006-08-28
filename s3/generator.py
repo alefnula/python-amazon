4298d749e4fc94db77ea556e0580b88d6ee64c7d
alefnula <alefnula@44b417e4-df1b-0410-81cb-fb11499e953d>
2006-08-28 14:16:08 +0000

    S3Generator implementation changes. Now it's much more similar to the S3Connection.

--- s3/generator.py
+++ s3/generator.py
@@ -1,6 +1,14 @@
 import s3
+import re
+import sha
+import hmac
 import time
+import base64
 import urllib
+
+PORTS_BY_SECURITY = { True: 443, False: 80 }
+
+DEFAULT_EXPIRES_IN = 60
 
 class S3Generator:
     """
@@ -9,8 +17,7 @@ class S3Generator:
     Objects of this class are used for generating authenticated URLs for accessing
     Amazon's S3 service
     """
-    DEFAULT_EXPIRES_IN = 60
-
+    
     def __init__(self, pub_key, priv_key, secure=True, host=s3.DEFAULT_HOST, port=None):
         self._pub_key = pub_key
         self._priv_key = priv_key
@@ -21,66 +28,67 @@ class S3Generator:
             self._port = port
         self._secure = secure
 
-        if not port:
-            port = PORTS_BY_SECURITY[is_secure]
-
         if (secure):
             self.protocol = 'https'
         else:
             self.protocol = 'http'
 
-        self.server_name = "%s:%d" % (host, port)
-        self.__expires_in = RestGenerator.DEFAULT_EXPIRES_IN
-        self.__expires = None
+        self.server_name = "%s:%d" % (self._host, self._port)
+        self._expires_in = DEFAULT_EXPIRES_IN
+        self._expires = None
 
     def set_expires_in(self, expires_in):
-        self.__expires_in = expires_in
-        self.__expires = None
+        self._expires_in = expires_in
+        self._expires = None
 
     def set_expires(self, expires):
-        self.__expires = expires
-        self.__expires_in = None
+        self._expires = expires
+        self._expires_in = None
 
-    def create_bucket(self, bucket, headers={}):
-        return self.generate_url('PUT', bucket, headers)
+    def create_bucket(self, name, headers={}):
+        """
+        Create a bucket.
+        
+        @param name:    Name for the new bucket
+        @type  name:    string
+        @param headers: Additional headers
+        @type  headers: dict
+        @return:        Authenticated URL for creating bucket
+        @rtype:         string
+        """
+        return self._generate('PUT', bucket=name, headers=headers)
 
-    def list_bucket(self, bucket, options={}, headers={}):
-        path = bucket
-        if options:
-            path += '?' + '&'.join(["%s=%s" % (param, urllib.quote_plus(options[param])) for param in options])
-
-        return self.generate_url('GET', path, headers)
+    def list_bucket(self, bucket, params={}, headers={}):
+        return self._generate('GET', bucket=bucket, params=params, headers=headers)
 
     def delete_bucket(self, bucket, headers={}):
-        return self.generate_url('DELETE', bucket, headers)
+        return self._generate('DELETE', bucket=bucket, headers=headers)
 
     def put(self, bucket, key, object, headers={}):
         headers.update(object.get_meta_for_headers())
-        return self.generate_url(
-                'PUT',
-                '%s/%s' % (bucket, urllib.quote_plus(key)), headers)
+        return self._generate('PUT', bucket=bucket, key=key, headers=headers)
 
     def get(self, bucket, key, headers={}):
-        return self.generate_url('GET', '%s/%s' % (bucket, urllib.quote_plus(key)), headers)
+        return self._generate('GET', bucket=bucket, key=key, headers=headers)
 
     def delete(self, bucket, key, headers={}):
-        return self.generate_url('DELETE', '%s/%s' % (bucket, urllib.quote_plus(key)), headers)
+        return self._generate('DELETE', bucket=bucket, key=key, headers=headers)
 
     def get_bucket_acl(self, bucket, headers={}):
-        return self.get_acl(bucket, '', headers)
+        return self.get_acl(bucket, None, headers)
 
-    def get_acl(self, bucket, key='', headers={}):
-        return self.generate_url('GET', '%s/%s?acl' % (bucket, urllib.quote_plus(key)), headers)
+    def get_acl(self, bucket, key=None, headers={}):
+        return self._generate('GET', acl=True, bucket=bucket, key=key, headers=headers)
 
     def put_bucket_acl(self, bucket, acl_xml_document, headers={}):
         return self.put_acl(bucket, '', acl_xml_document, headers)
 
     # don't really care what the doc is here.
     def put_acl(self, bucket, key, acl_xml_document, headers={}):
-        return self.generate_url('PUT', '%s/%s?acl' % (bucket, urllib.quote_plus(key)), headers)
+        return self._generate('PUT', acl=True, bucket=bucket, key=key, headers=headers)
 
     def list_all_my_buckets(self, headers={}):
-        return self.generate_url('GET', '', headers)
+        return self._generate('GET', headers=headers)
 
     def make_bare_url(self, bucket, key=''):
         return self.protocol + '://' + self.server_name + '/' + bucket + '/' + key
@@ -93,36 +101,48 @@ class S3Generator:
                       headers.get("Content-Type", ""),
                       headers.get("Date", ""),]
         auth_parts.extend([k + ":" + headers[k].strip() for k in xamzs])
-        # hmmm fali mi ona perverzija za ?acl i ?torrent
-        auth_parts.append(path)
+        
+        # Don't include anything after the first ?, unless there is an acl or torrent parameter
+        stripped_path = path.split('?')[0]
+        if re.search("[&?]acl($|=|&)", path):
+            stripped_path += "?acl"
+        elif re.search("[&?]torrent($|=|&)", path):
+            stripped_path += "?torrent"
+
+        auth_parts.append(stripped_path)
         auth_str = "\n".join(auth_parts)
         auth_str = base64.encodestring(
             hmac.new(self._priv_key, auth_str, sha).digest()).strip()
-        return "AWS %s:%s" % (self._pub_key, auth_str)
+        return urllib.quote_plus(auth_str)
 
-    def _headers(self, method, path, length=None, headers=None):
+    def _headers(self, method, path, length=None, headers=None, expires=None):
         if not headers:
             headers = {}
-        headers["Date"] = strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+        headers["Date"] = str(expires)
         if length is not None:
             headers["Content-Length"] = length
-        return urllib.quote_plus(self._auth_header_value(method, path, headers))
-##        headers["Authorization"] = self._auth_header_value(method, path, headers)
-##        return headers
+        return headers
 
-    def _params(self, params):
+    def _params(self, params, acl=False):
         p = ''
         if params:
-            p = '?' + urllib.urlencode(params)
+            if acl:
+                arg_div = '&'
+            else:
+                arg_div = '?'
+            p = arg_div + urllib.urlencode(params)
         return p
 
-    def _path(self, bucket=None, obj=None):
+    def _path(self, bucket=None, key=None, acl=False):
         if bucket is None:
-            return "/"
-        bucket = "/" + bucket
-        if obj is None:
-            return bucket
-        return bucket + "/" + urllib.quote(obj)
+            path = "/"
+        else:
+            path = "/" + bucket
+            if key is not None:
+                path += "/" + urllib.quote(key)
+        if acl:
+            path += '?acl'
+        return path
 
     def _io_len(self, io):
         if hasattr(io, "len"):
@@ -133,24 +153,27 @@ class S3Generator:
         io.seek(o_pos, 0)
         return length
 
-    def generate_url(self, method, path, headers):
+    def _generate(self, method, bucket=None, key=None,
+                  send_io=None, params=None, headers=None, acl=False):
         expires = 0
-        if self.__expires_in != None:
-            expires = int(time.time() + self.__expires_in)
-        elif self.__expires != None:
-            expires = int(self.__expires)
-        else:
-            raise "Invalid expires state"
+        if self._expires_in != None:
+            expires = int(time.time() + self._expires_in)
+        elif self._expires != None:
+            expires = int(self._expires)
 
-        encoded_canonical = self._headers(method, path, headers=headers)
-
+        path = self._path(bucket, key, acl)
+        length = None
+        if isinstance(headers, dict) and headers.has_key("Content-Length"):
+            length = headers["Content-Length"]
+        elif send_io is not None:
+            length = self._io_len(send_io)
+        headers = self._headers(method, path, length=length, headers=headers, expires=expires)
+        signature = self._auth_header_value(method, path, headers)
+        path += self._params(params, acl)
         if '?' in path:
             arg_div = '&'
         else:
             arg_div = '?'
+        query_part = "Signature=%s&Expires=%d&AWSAccessKeyId=%s" % (signature, expires, self._pub_key)
 
-        query_part = "Signature=%s&Expires=%d&AWSAccessKeyId=%s" % (encoded_canonical, expires, self._pub_key)
-
-        return self.protocol + '://' + self.server_name + '/' + path  + arg_div + query_part
-
-
+        return self.protocol + '://' + self.server_name + path + arg_div + query_part
